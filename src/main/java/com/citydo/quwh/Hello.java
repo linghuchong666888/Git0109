ad5edc02051d2ffae9df8c0f45dc20adbb932cb8
linghuchong <a13526880596@163.com>
2020-01-09 23:45:14 +0800

    BBBBBBBBBBBBBB

--- src/main/java/com/citydo/quwh/Hello.java
+++ src/main/java/com/citydo/quwh/Hello.java
@@ -3,5 +3,6 @@ package com.citydo.quwh;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world......");
+        System.out.println("BBBBBBBBBBBB");
     }
 }
